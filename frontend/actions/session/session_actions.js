010d0bc13489827ac1f3e3a08d15d8bdfb0037e1
ChristopherALee <christopheradamlee@gmail.com>
2018-02-02 17:10:20 -0500

    setup session actions and testing logging in functions in gobble.jsx

---
+++ frontend/actions/session/session_actions.js
@@ -0,0 +1,89 @@
+import * as SessionApiUtil from '../../util/session/session_api_util';
+
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
+export const RECEIVE_SIGNUP_ERRORS = 'RECEIVE_SIGNUP_ERRORS';
+export const DELETE_ERRORS = 'DELETE_ERRORS';
+export const LOG_OUT = 'LOG_OUT';
+
+export const receiveCurrentUser = (currentUser) => {
+  return {
+    type: RECEIVE_CURRENT_USER,
+    currentUser
+  };
+};
+
+export const receiveErrors = (errors) => {
+  return {
+    type: RECEIVE_SESSION_ERRORS,
+    errors
+  };
+};
+
+export const receiveSignUpErrors = (errors) => {
+  return {
+    type: RECEIVE_SIGNUP_ERRORS,
+    errors
+  };
+};
+
+export const resetState = () => {
+  return {
+    type: LOG_OUT
+  };
+};
+
+export const deleteErrors = () => {
+  return {
+    type: DELETE_ERRORS
+  };
+};
+
+export const deleteAllErrors = () => dispatch => {
+  return (
+    dispatch(deleteErrors())
+  );
+};
+
+export const signUp = user => dispatch => {
+  return (
+    SessionApiUtil.signUp(user).then(
+      user => {
+        return (
+          dispatch(receiveCurrentUser(user))
+        );
+      },
+      errors => {
+        dispatch(receiveSignUpErrors(errors.responseJSON));
+        return errors;
+      }
+    )
+  );
+};
+
+export const login = user => dispatch => {
+  return (
+    SessionApiUtil.login(user).then(
+      currentUser => {
+        dispatch(receiveCurrentUser(currentUser));
+        return currentUser;
+      },
+      errors => {
+        dispatch(receiveErrors(errors.responseJSON));
+        return errors;
+      }
+    )
+  );
+};
+
+export const logout = () => dispatch => {
+  return (
+    SessionApiUtil.logout().then(
+      (response) => {
+        dispatch(resetState());
+        dispatch(receiveCurrentUser(null));
+        return response;
+      }
+    )
+  );
+};
--- frontend/gobble.jsx
+++ frontend/gobble.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// TESTING PURPOSES START
+import * as SessionApiUtil from './util/session/session_api_util';
+
+window.signUp = SessionApiUtil.signUp;
+window.login = SessionApiUtil.login;
+window.logout = SessionApiUtil.logout;
+// TESTING ENDS
+
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   ReactDOM.render(<h1>gobble</h1>, root);
