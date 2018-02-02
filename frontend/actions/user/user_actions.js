829eada80b030d29d718f0a3bb40088d382c8fc8
ChristopherALee <christopheradamlee@gmail.com>
2018-02-02 17:07:55 -0500

    Setup user actions

---
+++ frontend/actions/user/user_actions.js
@@ -0,0 +1,52 @@
+import * as UserApiUtil from '../util/users/user_api_util';
+
+export const RECEIVE_ALL_USERS = 'RECEIVE_ALL_USERS';
+export const RECEIVE_SINGLE_USER = 'RECEIVE_SINGLE_USER';
+
+export const receiveAllUsers = (users) => {
+  return {
+    type: RECEIVE_ALL_USERS,
+    users
+  };
+};
+
+export const receiveSingleUser = (user) => {
+  return {
+    type: RECEIVE_SINGLE_USER,
+    user
+  };
+};
+
+export const fetchAllUsers = (users) => dispatch => {
+  return (
+    UserApiUtil.fetchUsers().then(
+      (users) => {
+        dispatch(receiveAllUsers(users));
+        return users;
+      }
+    )
+  );
+};
+
+export const fetchSingleUser = (user) => dispatch => {
+  return (
+    UserApiUtil.fetchSingleUser(user).then(
+      (user) => {
+        dispatch(receiveSingleUser(user));
+        return user;
+      }
+    )
+  );
+};
+
+export const updateUser = (user, username) => dispatch => {
+
+  return (
+    UserApiUtil.updateUser(user, username).then(
+      (user) => {
+        dispatch(receiveSingleUser(user));
+        return user;
+      }
+    )
+  );
+};
