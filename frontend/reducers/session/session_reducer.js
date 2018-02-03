c9dc2ee2db728cfe0994c637a6a1af0ea2411b00
ChristopherALee <christopheradamlee@gmail.com>
2018-02-03 14:54:28 -0500

    Created session reducer

---
+++ frontend/reducers/session/session_reducer.js
@@ -0,0 +1,22 @@
+import { RECEIVE_CURRENT_USER } from '../../actions/session/session_actions';
+import { RECEIVE_SINGLE_USER } from '../../actions/user/user_actions';
+
+const sessionReducer = (state = {currentUser: null}, action) => {
+  let newState;
+  Object.freeze(state);
+
+  switch (action.type) {
+    case RECEIVE_CURRENT_USER:
+      newState = Object.assign({}, state, {currentUser: action.currentUser});
+      return newState;
+    case RECEIVE_SINGLE_USER:
+      if (state.currentUser.username === action.user.username) {
+        newState = Object.assign({}, state, {currentUser: action.user});
+        return newState;
+      }
+    default:
+      return state;
+  }
+};
+
+export default sessionReducer;
