98a3b0d75bd9b3c9e310ea4fd51c2465cce5aa5d
ChristopherALee <christopheradamlee@gmail.com>
2018-02-04 15:30:34 -0500

    Added user reducer

---
+++ frontend/reducers/user/user_reducer.js
@@ -0,0 +1,25 @@
+import {
+  RECEIVE_ALL_USERS,
+  RECEIVE_SINGLE_USER } from '../../actions/user/user_actions';
+import { LOG_OUT } from '../../actions/session/session_actions';
+
+const userReducer = (state = {}, action) => {
+  let newState;
+  let authorName;
+
+  switch (action.type) {
+    case RECEIVE_ALL_USERS:
+      newState = Object.assign({}, state, action.users);
+      return newState;
+    case RECEIVE_SINGLE_USER:
+      newState = Object.assign({}, state, {[action.user.username]: action.user});
+      return newState;
+    case LOG_OUT:
+      newState = {};
+      return newState;
+    default:
+      return state;
+  }
+};
+
+export default userReducer;
