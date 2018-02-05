e3350f21e6dd556014a0b840124c72d0fd0cba1b
ChristopherALee <christopheradamlee@gmail.com>
2018-02-05 09:44:14 -0500

    Revised root reducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,8 +1,7 @@
 import { combineReducers } from 'redux';
+import entitiesReducer from './entities_reducer';
 import sessionReducer from './session/session_reducer';
 import errorsReducer from './errors_reducer';
-import userReducer from './user/user_reducer';
-import entitiesReducer from './entities_reducer';
 
 const rootReducer = combineReducers({
   entities: entitiesReducer,
