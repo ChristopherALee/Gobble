7c8cf71224942b1578cdecddb562aecf356283c2
ChristopherALee <christopheradamlee@gmail.com>
2018-02-05 10:01:47 -0500

    Created auth and protected routes

---
+++ frontend/util/protected_route.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Route, withRouter, Redirect } from 'react-router-dom';
+
+const Protected = ({component: Component, path, loggedIn}) => (
+  <Route path={path} render={(props) => (
+    loggedIn ? (
+      <Component {...props} />
+    ) : (
+      <Redirect to="/login" />
+    )
+  )}/>
+);
+
+const mapStateToProps = state => {
+  return {loggedIn: Boolean(state.session.currentUser)};
+};
+
+export const ProtectedRoute = withRouter(connect(mapStateToProps, null)(Protected));
