11fc5d7a6cc63d2b0bb6632641d1db81b4872ace
ChristopherALee <christopheradamlee@gmail.com>
2018-02-05 09:55:03 -0500

    Created session form container

---
+++ frontend/components/session/session_form_container.js
@@ -0,0 +1,32 @@
+import { connect } from 'react-redux';
+import SessionForm from './session_form';
+import { signUp, login, deleteAllErrors } from '../../actions/session/session_actions';
+import { fetchAllUsers } from '../../actions/user/user_actions';
+
+const mapStateToProps = (state, ownProps) => {
+  const formType = ownProps.location.pathname === '/signup' ? 'signup' : 'login';
+  const errors = ownProps.location.pathname === '/signup' ? state.errors.signUp.errors : state.errors.session.errors;
+
+  return {
+    loggedIn: Boolean(state.session.currentUser),
+    errors: errors,
+    formType: formType
+  };
+};
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+  const formType = ownProps.location.pathname === '/signup' ? 'signup' : 'login';
+  const action = formType === 'signup' ? signUp : login;
+
+  return {
+    processForm: (user) => dispatch(action(user)),
+    deleteAllErrors: () => dispatch(deleteAllErrors()),
+    login: (user) => dispatch(login(user)),
+    fetchAllUsers: () => dispatch(fetchAllUsers())
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(SessionForm);
