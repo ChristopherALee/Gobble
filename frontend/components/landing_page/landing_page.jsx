4d7a219fa251f5f5484a441372a2541a4c3e6396
ChristopherALee <christopheradamlee@gmail.com>
2018-02-06 16:08:21 -0500

    Resized image and intro text

--- frontend/components/landing_page/landing_page.jsx
+++ frontend/components/landing_page/landing_page.jsx
@@ -4,22 +4,24 @@ import {Link, withRouter} from 'react-router-dom';
 const LandingPage = (props) => {
   return (
     <div className="landing-page-container">
-      <div className="landing-page-image">
+      <div className="landing-page-contents">
+        <div className="landing-page-image">
 
-      </div>
+        </div>
 
-      <div className="landing-page-intro">
-        <h1>Let's Start Gobblin'</h1>
-        <p>When your team needs to kick off a project, hire a new employee, deploy some code, review a sales contract, finalize next year's budget, measure an A/B test, plan your next office opening, and more, Gobble has you covered.</p>
+        <div className="landing-page-intro">
+          <h1>Let's Start Gobblin'</h1>
+          <p>When your team needs to kick off a project, hire a new employee, deploy some code, review a sales contract, finalize next year's budget, measure an A/B test, plan your next office opening, and more, Gobble has you covered.</p>
 
-        <div className="landing-page-signup-login">
-          <Link to="/signup">
-            Get Started
-          </Link>
+          <div className="landing-page-signup-login">
+            <Link to="/signup" className="landing-page-signup">
+              Get Started
+            </Link>
 
-          <p>
-            Already Gobblin'? <Link to="/login">Sign In</Link>
-          </p>
+            <p className="already-gobblin">
+              Already Gobblin'? <Link to="/login" className="landing-page-login">Sign In</Link>
+            </p>
+          </div>
         </div>
       </div>
     </div>
