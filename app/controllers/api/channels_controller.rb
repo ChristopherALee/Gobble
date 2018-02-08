ebf62f16ec84991404f78fa65687cdcdc406121f
ChristopherALee <christopheradamlee@gmail.com>
2018-02-08 15:28:48 -0500

    Created channels controller

--- app/controllers/api/channels_controller.rb
+++ app/controllers/api/channels_controller.rb
@@ -8,12 +8,34 @@ class Api::ChannelsController < ApplicationController
   end
 
   def create
+    @channel = Channel.new(channel_params)
+
+    if @channel.save
+      render 'api/channels/show'
+    else
+      render json: @channel.errors.messages, status: 422
+    end
   end
 
   def update
+    @channel = Channel.find(params[:id])
+
+    if @channel && @channel.update
+      render 'api/channels/show'
+    else
+      render json: ['Cannot edit channel'], status: 403
+    end
   end
 
   def destroy
+    @channel = Channel.find(params[:id])
+
+    if @channel && @channel.creator_id == current_user.id
+      @channel.destroy
+      render 'api/channels/show'
+    else
+      render json: ['Cannot delete channel'], status: 403
+    end
   end
 
   private
