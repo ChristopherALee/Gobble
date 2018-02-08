4b7a12f2d73b72cf39eee441de9afe19b31647ef
ChristopherALee <christopheradamlee@gmail.com>
2018-02-08 17:29:33 -0500

    Created main logged in component layout for gobble. will store channels, messages and channel details

---
+++ frontend/components/main_gobble/main_gobble.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link, withRouter } from 'react-router-dom';
+
+class MainGobble extends React.Component {
+  constructor(props) {
+
+  }
+
+  render() {
+    return (
+      null
+    );
+  }
+}
+
+export default withRouter(MainGobble);
---
+++ frontend/components/main_gobble/main_gobble_container.js
@@ -0,0 +1,19 @@
+import { connect } from 'react-redux';
+import MainGobble from './main_gobble';
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+
+  };
+};
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+
+  };
+};
+
+export default connect(
+  null,
+  null
+)(MainGobble);
