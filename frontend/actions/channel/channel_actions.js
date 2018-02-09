cfbd7eec2dbd8ec57a4fccb556c0980533271be4
ChristopherALee <christopheradamlee@gmail.com>
2018-02-09 10:42:08 -0500

    Created channel actions

---
+++ frontend/actions/channel/channel_actions.js
@@ -0,0 +1,70 @@
+import * as ChannelApiUtil from '../../util/channels/channel_api_util';
+
+export const RECEIVE_ALL_CHANNELS = 'RECEIVE_ALL_CHANNELS';
+export const RECEIVE_SINGLE_CHANNEL = 'RECEIVE_SINGLE_CHANNEL';
+export const DELETE_CHANNEL = 'DELETE_CHANNEL';
+
+const receiveAllChannels = (channels) => {
+  return {
+    type: RECEIVE_ALL_CHANNELS,
+    channels
+  };
+};
+
+const receiveSingleChannel = (channel) => {
+  return {
+    type: RECEIVE_SINGLE_CHANNEL,
+    channel
+  };
+};
+
+const deleteChannel = (id) => {
+  return {
+    type: DELETE_CHANNEL,
+    channelId: id
+  };
+};
+
+export const fetchAllChannels = () => dispatch => {
+  return (
+    ChannelApiUtil.fetchAllChannels().then(
+      (channels) => {
+        dispatch(receiveAllChannels(channels));
+        return channels;
+      }
+    )
+  );
+};
+
+export const fetchSingleChannel = (id) => dispatch => {
+  return (
+    ChannelApiUtil.fetchSingleChannel(id).then(
+      (channel) => {
+        dispatch(receiveSingleChannel(channel));
+        return channel;
+      }
+    )
+  );
+};
+
+export const updateSingleChannel = (channel) => dispatch => {
+  return (
+    ChannelApiUtil.updateChannel(channel).then(
+      (channel) => {
+        dispatch(receiveSingleChannel(channel));
+        return channel;
+      }
+    )
+  );
+};
+
+export const removeChannel = (id) => dispatch => {
+  return (
+    ChannelApiUtil.deleteChannel(id).then(
+      (channel) => {
+        dispatch(deleteChannel(channel.id));
+        return channel;
+      }
+    )
+  );
+};
