cfb190ceaf97b9ac7794cfba5a3217b54780dbce
ChristopherALee <christopheradamlee@gmail.com>
2018-02-09 10:29:33 -0500

    Created membership api util

---
+++ frontend/util/memberships/membership_api_util.js
@@ -0,0 +1,36 @@
+export const fetchAllMemberships = () => {
+  return $.ajax({
+    method: 'GET',
+    url: '/api/memberships'
+  });
+};
+
+export const fetchSingleMembership = (id) => {
+  return $.ajax({
+    method: 'GET',
+    url: `/api/memberships/${id}`
+  });
+};
+
+export const createMembership = (membership) => {
+  return $.ajax({
+    method: 'POST',
+    url: '/api/memberships',
+    data: membership
+  });
+};
+
+export const updateMembership = (membership) => {
+  return $.ajax({
+    method: 'PATCH',
+    url: `/api/channels/${membership.id}`,
+    data: membership
+  });
+};
+
+export const deleteMembership = (id) => {
+  return $.ajax({
+    method: 'DELETE',
+    url: `/api/membership/${id}`
+  });
+};
