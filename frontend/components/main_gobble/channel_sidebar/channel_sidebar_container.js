577c6e695e323237fbe11eabe40a6d625aa0f1da
ChristopherALee <christopheradamlee@gmail.com>
2018-02-09 10:27:41 -0500

    Created sidebar component file and created channel api util file

---
+++ frontend/components/main_gobble/channel_sidebar/channel_sidebar_container.js
@@ -0,0 +1,15 @@
+import { connect } from 'react-redux';
+import ChannelSideBar from './channel_sidebar';
+
+const mapStateToProps = (state, ownProps) => {
+
+};
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ChannelSideBar);
