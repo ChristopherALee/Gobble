458c79e1ae05b08587515d0e93db4cca6a0a33d4
ChristopherALee <christopheradamlee@gmail.com>
2018-02-13 09:54:53 -0500

    Added channel menu to reducer

--- frontend/reducers/ui_reducer.js
+++ frontend/reducers/ui_reducer.js
@@ -1,6 +1,8 @@
 import {
   RENDER_MENU,
-  REMOVE_MENU } from '../actions/ui/menu_actions';
+  REMOVE_MENU,
+  RENDER_CREATE_CHANNEL_MENU,
+  REMOVE_CREATE_CHANNEL_MENU } from '../actions/ui/menu_actions';
 import { LOG_OUT } from '../actions/session/session_actions';
 
 const uiReducer = (state = {}, action) => {
@@ -13,6 +15,12 @@ const uiReducer = (state = {}, action) => {
       return newState;
     case REMOVE_MENU:
       newState = Object.assign({}, state, {menu: action.render});
+      return newState;
+    case RENDER_CREATE_CHANNEL_MENU:
+      newState = Object.assign({}, state, {createChannelMenu: action.render});
+      return newState;
+    case REMOVE_CREATE_CHANNEL_MENU:
+      newState = Object.assign({}, state, {createChannelMenu: action.render});
       return newState;
     case LOG_OUT:
       newState = {};
