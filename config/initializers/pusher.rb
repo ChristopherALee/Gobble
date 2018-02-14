768a022185a374933de85d6eeb45387d18dd47e0
ChristopherALee <christopheradamlee@gmail.com>
2018-02-14 11:20:12 -0500

    Created pusher.rb file in config/initializers

---
+++ config/initializers/pusher.rb
@@ -0,0 +1,8 @@
+require 'pusher'
+
+Pusher.app_id = '475403'
+Pusher.key = '416ebb2d74bf61955f19'
+Pusher.secret = '02ca65d81dc429baa722'
+Pusher.cluster = 'us2'
+Pusher.logger = Rails.logger
+Pusher.encrypted = true
