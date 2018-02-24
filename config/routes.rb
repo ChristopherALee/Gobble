b113fff7a9690312161b40b4332cf886d2327e23
ChristopherALee <christopheradamlee@gmail.com>
2018-02-24 16:29:14 -0500

    Added direct messages routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,10 +10,15 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     resources :users, only: [:index, :create, :show, :update]
     resource :session, only: [:create, :destroy]
+
     resources :channels, only: [:index, :create, :show, :update, :destroy]
     resources :memberships, only: [:index, :create, :show, :destroy]
     resources :messages, only: [:index, :create, :show, :update, :destroy]
 
     get '/channels/:name/messages', to: 'channels#messages'
+
+    resources :direct_message_channels, only: [:index, :show, :create, :destroy]
+    resources :direct_message_channel_memberships, only: [:index, :create, :show, :destroy]
+    resources :direct_messages, only: [:index, :create, :show, :update, :destroy]
   end
 end
