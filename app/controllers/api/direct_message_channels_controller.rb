d0ba27905814b8166b4bf6f3b9447b6289a95439
ChristopherALee <christopheradamlee@gmail.com>
2018-02-24 16:22:54 -0500

    Created direct message channel controller

---
+++ app/controllers/api/direct_message_channels_controller.rb
@@ -0,0 +1,37 @@
+class Api::DirectMessageChannelsController < ApplicationController
+  def index
+    @direct_message_channels = DirectMessageChannels.all
+  end
+
+  def show
+    @direct_message_channel = DirectMessageChannel.find(params[:id])
+  end
+
+  def create
+    @direct_message_channel = DirectChannel.new(direct_channel_params)
+    @direct_message_channel.creator_id = current_user.id
+
+    if @direct_message_channel.save
+      Pusher.trigger('sidebar_dm', 'dm_created', {})
+      render 'api/direct_message_channels/show'
+    else
+      render json: @direct_message_channel.errors.messages, status: 422
+    end
+  end
+
+  def destroy
+    @direct_message_channel = DirectMessageChannel.find(params[:id])
+
+    if @direct_message_channel && @direct_message_channel.creator_id == current_user.id
+      @direct_message_channel.destroy
+      render 'api/direct_message_channels/show'
+    else
+      render json: ['Cannot delete direct message'], status: 403
+    end
+  end
+
+  private
+  def direct_channel_params
+    params.require(:direct_message_channel).permit(:id)
+  end
+end
