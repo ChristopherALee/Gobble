388919b20b4d7d18494e11aa34977d75e9e525a3
ChristopherALee <christopheradamlee@gmail.com>
2018-03-04 17:15:27 -0500

    Added new users and channels to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,13 +17,36 @@ DirectMessage.destroy_all
 
 # users
 user1 = User.create({username: 'guest', password: 'password123'})
-user2 = User.create({username: 'chris', password: 'password123'})
-user3 = User.create({username: 'lily', password: 'password123'})
+user2 = User.create({username: 'BigBird', password: 'password123'})
+user3 = User.create({username: 'Tweety', password: 'password123'})
+user4 = User.create({username: 'Woodstock', password: 'password123'})
+user5 = User.create({username: 'Daffy', password: 'password123'})
+user6 = User.create({username: 'Donald', password: 'password123'})
+user7 = User.create({username: 'Woody', password: 'password123'})
+user8 = User.create({username: 'ToucanSam', password: 'password123'})
+user9 = User.create({username: 'Daisy', password: 'password123'})
+user10 = User.create({username: 'Iago', password: 'password123'})
+user11 = User.create({username: 'Zazu', password: 'password123'})
+user12 = User.create({username: 'Tootsie', password: 'password123'})
+user13 = User.create({username: 'Skipper', password: 'password123'})
+user14 = User.create({username: 'Private', password: 'password123'})
+user15 = User.create({username: 'Rico', password: 'password123'})
+user16 = User.create({username: 'Kowalski', password: 'password123'})
+user17 = User.create({username: 'Kevin', password: 'password123'})
+user18 = User.create({username: 'Huey', password: 'password123'})
+user19 = User.create({username: 'Dewey', password: 'password123'})
+user20 = User.create({username: 'Louie', password: 'password123'})
 
 # channels
 channel1 = Channel.create({name: 'bread', creator_id: user1.id, purpose: 'we like bread', topic: 'bread on the 5th floor!'})
 channel2 = Channel.create({name: 'hangouts', creator_id: user2.id, purpose: 'find random places to perch', topic: '8th street park! now!!'})
 channel3 = Channel.create({name: 'windows', creator_id: user3.id, purpose: 'how do they work?', topic: 'stop crashing into windows'})
+channel4 = Channel.create({name: 'pigeons', creator_id: user1.id, purpose: 'we are pigeons!', topic: 'New York born and raised'})
+channel5 = Channel.create({name: 'seagulls', creator_id: user2.id, purpose: 'we are seagulls!', topic: 'Saturday night party: Pier 88'})
+channel6 = Channel.create({name: 'chickens', creator_id: user3.id, purpose: 'we are chickens!', topic: "who keeps taking ms.cluck's eggs?"})
+channel7 = Channel.create({name: 'ducks', creator_id: user1.id, purpose: 'we are ducks!', topic: 'river quackin'})
+channel8 = Channel.create({name: 'migration', creator_id: user2.id, purpose: 'Seasonal Migration Announcements', topic: 'Prep for Spring migration!'})
+channel9 = Channel.create({name: 'egg-daycare', creator_id: user3.id, purpose: 'find egg-sitters for your nest', topic: 'egg-stealers will not be tolerated!'})
 
 # memberships
 membership1 = Membership.create({member_id: user1.id, channel_id: channel1.id})
