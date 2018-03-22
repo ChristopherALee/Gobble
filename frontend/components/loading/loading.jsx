b0437f75238b17e350815e695b94f8661d1f2075
ChristopherALee <christopheradamlee@gmail.com>
2018-03-22 16:06:47 -0400

    Revised quote styling

--- frontend/components/loading/loading.jsx
+++ frontend/components/loading/loading.jsx
@@ -6,55 +6,52 @@ class Loading extends React.Component {
   }
 
   render() {
-    if (this.props.loading) {
-      return (
-        <div id="loading-screen">
-          <section className="loading-sidebar">
-            <div className="loading-sidebar-container">
-              <div className="loading-header">
-                <div className="loading-title" />
-                <div className="loading-username" />
-              </div>
-
-              <div className="loading-channel-list-container">
-                <div className="loading-channel-header" />
-                <ul className="loading-channel-list">
-                  <li className="loading-channel-list-item" />
-                  <li className="loading-channel-list-item3" />
-                  <li className="loading-channel-list-item2" />
-                  <li className="loading-channel-list-item2" />
-                  <li className="loading-channel-list-item3" />
-                </ul>
-              </div>
-
-              <div className="loading-channel-list-container">
-                <div className="loading-channel-header" />
-                <ul className="loading-channel-list">
-                  <li className="loading-channel-list-item2" />
-                  <li className="loading-channel-list-item" />
-                  <li className="loading-channel-list-item3" />
-                  <li className="loading-channel-list-item2" />
-                  <li className="loading-channel-list-item" />
-                  <li className="loading-channel-list-item2" />
-                  <li className="loading-channel-list-item" />
-                </ul>
-              </div>
+    return (
+      <div id="loading-screen">
+        <section className="loading-sidebar">
+          <div className="loading-sidebar-container">
+            <div className="loading-header">
+              <div className="loading-title" />
+              <div className="loading-username" />
             </div>
-          </section>
 
-          <section className="loading-messages">
-            <div className="loading-container">
-              <div id="loading-duck" />
-              <p className="loading-text">
-                Quack quack (loading) quack quack...
-              </p>
+            <div className="loading-channel-list-container">
+              <div className="loading-channel-header" />
+              <ul className="loading-channel-list">
+                <li className="loading-channel-list-item" />
+                <li className="loading-channel-list-item3" />
+                <li className="loading-channel-list-item2" />
+                <li className="loading-channel-list-item2" />
+                <li className="loading-channel-list-item3" />
+              </ul>
             </div>
-          </section>
-        </div>
-      );
-    } else {
-      return null;
-    }
+
+            <div className="loading-channel-list-container">
+              <div className="loading-channel-header" />
+              <ul className="loading-channel-list">
+                <li className="loading-channel-list-item2" />
+                <li className="loading-channel-list-item" />
+                <li className="loading-channel-list-item3" />
+                <li className="loading-channel-list-item2" />
+                <li className="loading-channel-list-item" />
+                <li className="loading-channel-list-item2" />
+                <li className="loading-channel-list-item" />
+              </ul>
+            </div>
+          </div>
+        </section>
+
+        <section className="loading-messages">
+          <div className="loading-container">
+            <div id="loading-duck" />
+            <div className="loading-text">
+              <p className="loading-quote">You're deth-picable!</p>
+              <p className="loading-quote-author">- Daffy Duck</p>
+            </div>
+          </div>
+        </section>
+      </div>
+    );
   }
 }
 
