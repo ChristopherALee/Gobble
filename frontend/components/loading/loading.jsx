d157c23f31cd9d62f55db31c66bcf3ef4710675f
ChristopherALee <christopheradamlee@gmail.com>
2018-03-23 15:58:06 -0400

    Uncommented conditional code for loading

--- frontend/components/loading/loading.jsx
+++ frontend/components/loading/loading.jsx
@@ -6,82 +6,81 @@ class Loading extends React.Component {
   }
 
   render() {
-    // if (this.props.loading) {
-    return (
-      <div id="loading-screen">
-        <section className="loading-sidebar">
-          <div className="loading-sidebar-container">
-            <div className="loading-header">
-              <div className="loading-title-menu">
-                <div className="loading-title" />
-                <div className="loading-menu-button" />
+    if (this.props.loading) {
+      return (
+        <div id="loading-screen">
+          <section className="loading-sidebar">
+            <div className="loading-sidebar-container">
+              <div className="loading-header">
+                <div className="loading-title-menu">
+                  <div className="loading-title" />
+                  <div className="loading-menu-button" />
+                </div>
+
+                <div className="loading-status-username">
+                  <div className="loading-status" />
+                  <div className="loading-username" />
+                </div>
               </div>
 
-              <div className="loading-status-username">
-                <div className="loading-status" />
-                <div className="loading-username" />
+              <div className="loading-channel-list-container">
+                <div className="loading-channel-header" />
+                <ul className="loading-channel-list">
+                  <li className="loading-channel-list-item" />
+                  <li className="loading-channel-list-item3" />
+                  <li className="loading-channel-list-item2" />
+                  <li className="loading-channel-list-item2" />
+                  <li className="loading-channel-list-item3" />
+                </ul>
+              </div>
+
+              <div className="loading-channel-list-container">
+                <div className="loading-channel-header" />
+                <ul className="loading-channel-list">
+                  <li className="loading-channel-list-item2" />
+                  <li className="loading-channel-list-item" />
+                  <li className="loading-channel-list-item3" />
+                  <li className="loading-channel-list-item2" />
+                  <li className="loading-channel-list-item" />
+                  <li className="loading-channel-list-item2" />
+                  <li className="loading-channel-list-item" />
+                </ul>
+              </div>
+            </div>
+          </section>
+
+          <section className="loading-messages">
+            <div className="loading-messages-header">
+              <div className="loading-messages-header-left">
+                <div className="loading-messages-header-title" />
+                <div className="loading-messages-header-description" />
+              </div>
+
+              <div className="loading-messages-header-right">
+                <div className="loading-messages-header-right-item1" />
+                <div className="loading-messages-header-right-item2" />
+                <div className="loading-messages-header-right-item1" />
+                <div className="loading-messages-header-right-item1" />
+                <div className="loading-messages-header-right-item1" />
               </div>
             </div>
 
-            <div className="loading-channel-list-container">
-              <div className="loading-channel-header" />
-              <ul className="loading-channel-list">
-                <li className="loading-channel-list-item" />
-                <li className="loading-channel-list-item3" />
-                <li className="loading-channel-list-item2" />
-                <li className="loading-channel-list-item2" />
-                <li className="loading-channel-list-item3" />
-              </ul>
-            </div>
-
-            <div className="loading-channel-list-container">
-              <div className="loading-channel-header" />
-              <ul className="loading-channel-list">
-                <li className="loading-channel-list-item2" />
-                <li className="loading-channel-list-item" />
-                <li className="loading-channel-list-item3" />
-                <li className="loading-channel-list-item2" />
-                <li className="loading-channel-list-item" />
-                <li className="loading-channel-list-item2" />
-                <li className="loading-channel-list-item" />
-              </ul>
-            </div>
-          </div>
-        </section>
-
-        <section className="loading-messages">
-          <div className="loading-messages-header">
-            <div className="loading-messages-header-left">
-              <div className="loading-messages-header-title" />
-              <div className="loading-messages-header-description" />
-            </div>
-
-            <div className="loading-messages-header-right">
-              <div className="loading-messages-header-right-item1" />
-              <div className="loading-messages-header-right-item2" />
-              <div className="loading-messages-header-right-item1" />
-              <div className="loading-messages-header-right-item1" />
-              <div className="loading-messages-header-right-item1" />
-            </div>
-          </div>
-
-          <div className="loading-container">
-            <div className="loading-content">
-              <div id="loading-duck" />
-              <div className="loading-text">
-                <p className="loading-quote">You're deth-picable!</p>
-                <p className="loading-quote-author">- Daffy Duck</p>
+            <div className="loading-container">
+              <div className="loading-content">
+                <div id="loading-duck" />
+                <div className="loading-text">
+                  <p className="loading-quote">You're deth-picable!</p>
+                  <p className="loading-quote-author">- Daffy Duck</p>
+                </div>
               </div>
             </div>
-          </div>
-        </section>
-      </div>
-    );
+          </section>
+        </div>
+      );
+    } else {
+      return null;
+    }
   }
-  // else {
-  //   return null;
-  // }
-  // }
 }
 
 export default Loading;
