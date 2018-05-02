45a291e9e7c3219db1e3b6a525cbc69c600803ee
ChristopherALee <christopheradamlee@gmail.com>
2018-05-02 16:10:46 -0400

    Adjusted landing page styling - description spacing

--- frontend/components/landing_page/landing_page.jsx
+++ frontend/components/landing_page/landing_page.jsx
@@ -1,30 +1,36 @@
-import React from 'react';
-import {Link, withRouter} from 'react-router-dom';
+import React from "react";
+import { Link, withRouter } from "react-router-dom";
 
-const LandingPage = (props) => {
+const LandingPage = props => {
   return (
     <div className="landing-page-container">
       <div className="landing-page-contents">
-        <div className="landing-page-image">
-
-        </div>
+        <div className="landing-page-image" />
 
         <div className="landing-page-intro">
           <h1>Let's Start Gobblin'</h1>
+
           <p className="intro-text">
-            Whether you quack, chirp, or gobble, staying in contact with your closest winged buddies (or co-workers) has never been easier. Schedule group flights? Lamp-post hang-outs? Bi-weekly bread buffets at the park? We've got you covered. <br></br> <strong>Start Gobblin', with Gobble.</strong>
+            Whether you quack, chirp, or gobble, staying in contact with your
+            closest winged buddies (or co-workers) has never been easier.
+            Schedule group flights? Lamp-post hang-outs? Bi-weekly bread buffets
+            at the park? We've got you covered.
+          </p>
+
+          <p className="intro-text2">
+            <strong>Start Gobblin', with Gobble.</strong>
           </p>
 
           <div className="landing-page-signup-login">
             <Link to="/signup">
-              <button className="landing-page-signup">
-                GET STARTED
-              </button>
+              <button className="landing-page-signup">GET STARTED</button>
             </Link>
 
-
             <p className="already-gobblin">
-              Already Gobblin'? <Link to="/login" className="landing-page-login">Sign In</Link>
+              Already Gobblin'?{" "}
+              <Link to="/login" className="landing-page-login">
+                Sign In
+              </Link>
             </p>
           </div>
         </div>
